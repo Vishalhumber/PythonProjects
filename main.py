752b370219aa589f1e5613c95060ac831aa75de5
Vishalhumber <n01496099@humbermail.ca>
2022-08-16 23:05:32 -0400

    firstCode

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to the rollercoaster")
+height= int(input("What is your height in cm? "))
+if height > 120:
+    print("you can ride the rollercoaster!")
+else:
+    print("sorry you have grow taller before you ride")
